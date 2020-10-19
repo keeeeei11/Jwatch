efd402a3af0f7e9243f56588b808f7c3d089b403
“kekekeigo1120” <t.keikei.0817@gmail.com>
2020-10-20 02:09:38 +0900

    タイトルとURLの設定

--- vue.config.js
+++ vue.config.js
@@ -20,31 +20,130 @@ module.exports = {
     // template is inferred to be `public/subpage.html`
     // and falls back to `public/index.html` if not found.
     // Output filename is inferred to be `subpage.html`.
-    answerquestion: "src/pages/answerquestion/main.js",
-    answerreception: "src/pages/answerreception/main.js",
-    deleteAccount: "src/pages/deleteAccount/main.js",
-    inquiry: "src/pages/inquiry/main.js",
-    introduction: "src/pages/introduction/main.js",
-    login: "src/pages/login/main.js",
-    loginAnonymous: "src/pages/loginAnonymous/main.js",
-    loginComplete: "src/pages/loginComplete/main.js",
-    loginEmail: "src/pages/loginEmail/main.js",
-    loginGoogle: "src/pages/loginGoogle/main.js",
-    loginTwitter: "src/pages/loginTwitter/main.js",
-    mainpage: "src/pages/mainpage/main.js",
-    mypage: "src/pages/mypage/main.js",
-    popular: "src/pages/popular/main.js",
-    posting: "src/pages/posting/main.js",
-    question: "src/pages/question/main.js",
-    reportpage: "src/pages/reportpage/main.js",
-    request: "src/pages/request/main.js",
-    searchinformation: "src/pages/searchinformation/main.js",
-    usage: "src/pages/usage/main.js",
-    inquirymail: "src/pages/inquirymail/main.js",
-    management: "src/pages/management/main.js",
-    reportlist: "src/pages/reportlist/main.js",
-    viewinformations: "src/pages/viewinformations/main.js",
-    warning: "src/pages/warning/main.js",
+    answerquestion: {
+      entry:"src/pages/answerquestion/main.js",
+      filename:"answerquestion/index.html",
+      title:"質問に答える！ Jwatch Jリーグ観戦情報サイト"
+    },
+    answerreception: {
+      entry:"src/pages/answerreception/main.js",
+      filename:"answerreception/index.html",
+      title:"回答受付中の質問一覧 Jwatch Jリーグ観戦情報サイト",
+    },
+    deleteAccount: {
+      entry:"src/pages/deleteAccount/main.js",
+      filename:"deleteaccount/index.html",
+    },
+    inquiry: {
+      entry:"src/pages/inquiry/main.js",
+      filename:"inquiry/index.html",
+      title:"ご意見＆ご要望はこちら Jwatch Jリーグ観戦情報サイト"
+    },
+    inquirymail: {
+      entry:"src/pages/inquirymail/main.js",
+      filename:"inquirymail/index.html",
+      title:"お問い合わせメール確認ページ"
+    },
+    introduction: {
+      entry:"src/pages/introduction/main.js",
+      filename:"introduction/index.html",
+      title:"管理人の紹介 Jwatch Jリーグ観戦情報サイト"
+    },
+    login: {
+      entry:"src/pages/login/main.js",
+      filename:"login/index.html",
+      title:"ログイン画面 Jwatch Jリーグ観戦情報サイト"
+    },
+    loginAnonymous: {
+      entry:"src/pages/loginAnonymous/main.js",
+      filename:"loginAnonymous/index.html",
+      title:"ログイン画面 Jwatch Jリーグ観戦情報サイト"
+    },
+    loginComplete: {
+      entry:"src/pages/loginComplete/main.js",
+      filename:"loginComplete/index.html",
+      title:"ログイン画面 Jwatch Jリーグ観戦情報サイト"
+    },
+    loginEmail: {
+      entry:"src/pages/loginEmail/main.js",
+      filename:"loginEmail/index.html",
+      title:"ログイン画面 Jwatch Jリーグ観戦情報サイト"
+    },
+    loginGoogle: {
+      entry:"src/pages/loginGoogle/main.js",
+      filename:"loginGoogle/index.html",
+      title:"ログイン画面 Jwatch Jリーグ観戦情報サイト"
+    },
+    loginTwitter: {
+      entry:"src/pages/loginTwitter/main.js",
+      filename:"loginTwitter/index.html",
+      title:"ログイン画面 Jwatch Jリーグ観戦情報サイト"
+    },
+    mainpage: {
+      entry:"src/pages/mainpage/main.js",
+      filename:"mainpage/index.html",
+      title:"Jwatch Jリーグ観戦情報サイト"
+    },
+    management: {
+      entry:"src/pages/management/main.js",
+      filename:"management/index.html",
+      title:"管理画面トップ"
+    },
+    mypage: {
+      entry:"src/pages/mypage/main.js",
+      filename:"mypage/index.html",
+      title:"〇〇さんのマイページ Jwatch Jリーグ観戦情報サイト"
+    },
+    popular: {
+      entry:"src/pages/popular/main.js",
+      filename:"popular/index.html",
+      title:"人気の投稿 Jwatch Jリーグ観戦情報サイト"
+    },
+    posting: {
+      entry:"src/pages/posting/main.js",
+      filename:"posting/index.html",
+      title:"投稿画面 Jwatch Jリーグ観戦情報サイト"
+    },
+    question: {
+      entry:"src/pages/question/main.js",
+      filename:"question/index.html",
+      title:"Q&A Jwatch Jリーグ観戦情報サイト"
+    },
+    reportlist: {
+      entry:"src/pages/reportlist/main.js",
+      filename:"reportlist/index.html",
+      title:"通報一覧確認ページ"
+    },
+    reportpage: {
+      entry:"src/pages/reportpage/main.js",
+      filename:"reportpage/index.html",
+      title:"通報画面 Jwatch Jリーグ観戦情報サイト"
+    },
+    request: {
+      entry:"src/pages/request/main.js",
+      filename:"request/index.html",
+      title:"こんな情報が欲しい！ Jwatch Jリーグ観戦情報サイト"
+    },
+    searchinformation: {
+      entry:"src/pages/searchinformation/main.js",
+      filename:"searchinformation/index.html",
+      title:"情報を探す！ Jwatch Jリーグ観戦情報サイト"
+    },
+    usage: {
+      entry:"src/pages/usage/main.js",
+      filename:"usage/index.html",
+      title:"詳しい使い方 Jwatch Jリーグ観戦情報サイト"
+    },
+    viewinformations: {
+      entry:"src/pages/viewinformations/main.js",
+      filename:"viewinformations/index.html",
+      title:"情報を見る！ Jwatch Jリーグ観戦情報サイト"
+    },
+    warning: {
+      entry:"src/pages/warning/main.js",
+      filename:"warning/index.html",
+      title:"投稿禁止対象の行為について Jwatch Jリーグ観戦情報サイト"
+    },
   },
   configureWebpack: {
     devtool: process.env.NODE_ENV === "production" ? "(none)" : "source-map",
