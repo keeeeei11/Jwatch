8d216b1d27178e0fad1bd9e1a20669b2b0e11e8c
“kekekeigo1120” <t.keikei.0817@gmail.com>
2020-10-23 03:26:36 +0900

    各ページタイトルのコンポーネント作成

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="page-title">
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'PageTitle',
+  data(){
+    return{
+      title:"",
+      description:"",
+    }
+  }
+}
+</script>
+
+<style>
+.page-title{
+    margin-left: 20%;
+    margin-top: 250px;
+}
+.page-title h1{
+    font-size: 48px;
+}
+
+.page-title p{
+    font-size: 24px;
+}
+
+@media (max-width:959px)
+{
+.page-title{
+    margin-left: 10%;
+}
+}
+
+@media (max-width:559px)
+{
+.page-title h1{
+    font-size: 36px;
+}
+
+.page-title p{
+    font-size: 18px;
+}
+}
+</style>
---
+++ src/components/PostBox.vue
@@ -0,0 +1,14 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name:'PostBox'
+
+}
+</script>
+
+<style>
+
+</style>
