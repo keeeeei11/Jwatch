f332c4fac3b15d322d4014804993f98f8d27d2f2
“kekekeigo1120” <t.keikei.0817@gmail.com>
2020-11-02 16:55:38 +0900

    データの取得＆ソート機能実装

--- src/pages/inquirymail/App.vue
+++ src/pages/inquirymail/App.vue
@@ -4,11 +4,11 @@
   <header>
     <div class="header-contents">
       <div class="header-title">
-        <a href="http://127.0.0.1:5501/public/usermanagement/management/management.html">管理者画面</a>
+        <a href="https://jwatch-8411c.web.app/management/index.html">管理者画面</a>
       </div>
       <div class="header-menu">
-        <a href="http://127.0.0.1:5501/public/usermanagement/reportlist/reportlist.html">通報一覧</a>
-        <a href="http://127.0.0.1:5501/public/usermanagement/inquirymail/inquirymail.html">お問い合わせメール</a>
+        <a href="https://jwatch-8411c.web.app/reportlist/index.html">通報一覧</a>
+        <a href="https://jwatch-8411c.web.app/inquirymail/index.html">お問い合わせメール</a>
       </div>
     </div>
   </header>
@@ -16,80 +16,48 @@
     <div class="inquiry-title">
       <h2>お問い合わせメール</h2>
     </div>
-    <div class="inquiry-contents">
+    <select name="" @change="sortData()" v-model="sortValue">
+      <option value="newest">新しい順</option>
+      <option value="oldest">古い順</option>
+    </select>
+    <div v-for="(inquirySingleData, index) in inquiryMultipleData" :key="inquirySingleData.index">
+      <div class="inquiry-contents">
       <div class="inquiry-example-contents">
         <div class="inquiry-basic-information">
           <div class="inquiry-basic-information-top">
               <div class="inquiry-name">
-                  <p id="inquiry_username">〇〇さん</p>
+                  <p>{{ inquirySingleData.name }}</p>
+                  <p>{{ index }}</p>
               </div>
               <div class="inquiry-email">
-                <p id="inquiry_email">〇〇@gmail.com</p>
+                <p>{{ inquirySingleData.mailAddress }}</p>
               </div>
-            </div>
-            <div class="inquiry-basic-information-bottom">
+          </div>
+          <div class="inquiry-basic-information-bottom">
               <div class="inquiry-date">
-                  <p id="inquiry_date">2000/1/1</p>
+                  <p>{{ inquirySingleData.created }}</p>
               </div>
           </div>
         </div>
         <div class="inquiry-main-content">
             <div class="inquiry-title">
-                <p id="inquiry_title">次回試合情報を記載してください</p>
+                <p>{{ inquirySingleData.title }}</p>
             </div>
             <div class="inquiry-text">
-                <p id="inquiry_body">次回試合情報(日程,対戦相手など)を記載していただけると、より一層便利になると思うのですがどうでしょうか？</p>
+                <p>{{ inquirySingleData.body }}</p>
             </div>
         </div>
         <div class="inquiry-evaluation">
           <div class="inquiry-evaluation-contents">
               <div class="inquiry-delete">
-                <p id="inquiry_execute">削除する</p>
+                <button @click="deleteData">このお問い合わせを削除する</button>
               </div>
           </div>
-      </div>
+        </div>
       </div>
     </div>
-      <div class="inquiry-example-contents">
-        <div class="inquiry-basic-information">
-          <div class="inquiry-basic-information-top">
-              <div class="inquiry-name">
-                  <p id="inquiry_username">〇〇さん</p>
-              </div>
-              <div class="inquiry-email">
-                <p id="inquiry_email">〇〇@gmail.com</p>
-              </div>
-            </div>
-            <div class="inquiry-basic-information-bottom">
-              <div class="inquiry-date">
-                  <p id="inquiry_date">2000/1/1</p>
-              </div>
-          </div>
-        </div>
-        <div class="inquiry-main-content">
-            <div class="inquiry-title">
-                <p id="inquiry_title">次回試合情報を記載してください</p>
-            </div>
-            <div class="inquiry-text">
-                <p id="inquiry_body">次回試合情報(日程,対戦相手など)を記載していただけると、より一層便利になると思うのですがどうでしょうか？</p>
-            </div>
-        </div>
-        <div class="inquiry-evaluation">
-          <div class="inquiry-evaluation-contents">
-              <div class="inquiry-delete">
-                  <p id="inquiry_execute">削除する</p>
-              </div>
-          </div>
-        </div>
-      </div>
-      <div class="inquiry-popup">
-        <section id="inquiry_reconfirmation" class="hidden">
-            <p>このお問い合わせ内容を削除してもよろしいですか？</p>
-            <p id="inquiry_cancel">戻る</p>
-            <a href="#" id="inquiry_btn">削除する</a>
-        </section>
-      </div>
-      <div id="reconfirmation_cover" class="hidden"></div>
+    </div>
+  <MoveTopBtn></MoveTopBtn>
   </main>
   </div>
 </div>
@@ -97,16 +65,122 @@
 
 <script>
 // あとで有効にする
-// import firebase from "firebase";
+import firebase from "firebase";
 import "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
 import myFirstMixin from '../../mixin/myFirstMixin';
+import MoveTopBtn from '../../components/MoveTopBtn.vue';
 export default {
+  props:{},
+  data(){
+    return{
+      // お問い合わせのデータを格納
+      inquiryMultipleData:[],
+      sortValue:localStorage.getItem("sortkey"),
+    }
+  },
   mixins:[
     myFirstMixin
   ],
+  components:{
+    MoveTopBtn
+  },
+  methods:{
+      // 初回訪問時&ページ更新時にデータを取得する
+    getData: function(){
+      if(this.sortValue === "newest"){
+        const db = firebase.firestore();
+        const inquiryData = db.collection("inquiries")
+        const displayData = inquiryData.orderBy('created','desc').get()
+        .then(querySnapshot => {
+          // docにcloud firestoreからのデータが格納されている
+            querySnapshot.forEach((doc) => {
+            console.log(this.inquiryMultipleData.push(doc.data()));
+              localStorage.setItem("sortkey", this.sortValue)
+            // inquiryData.add({
+            //   index:index
+            // })
+          })
+        })
+        .catch(function(error) {
+          console.log("Error getting documents: ", error);
+          console.log(displayData)
+        });
+      } else if (this.sortValue === "oldest"){
+          console.log(this.sortValue)
+          const db = firebase.firestore();
+          const inquiryData = db.collection("inquiries")
+          const oldestDisplayData = inquiryData.orderBy('created').get()
+          .then(querySnapshot => {
+            querySnapshot.forEach((doc) => {
+                console.log(this.inquiryMultipleData.push(doc.data()));
+                localStorage.setItem("sortkey", this.sortValue)
+              // inquiryData.add({
+              //   index:index
+              // })
+      })
+      })
+      .catch(function(error) {
+        console.log("Error getting documents: ", error);
+        console.log(oldestDisplayData)
+        });
+      } else {
+        return
+      }
+    },
+    // selectタグの操作時に実行する。ソート後にページを更新してデータを表示させる。。
+    sortData: function(){
+        if(this.sortValue === "newest"){
+            const db = firebase.firestore();
+            const inquiryData = db.collection("inquiries")
+            const newestDisplayData = inquiryData.orderBy('created','desc').get()
+            .then(querySnapshot => {
+              querySnapshot.forEach((doc) => {
+                  console.log(this.inquiryMultipleData.push(doc.data()));
+                  localStorage.setItem("sortkey", this.sortValue)
+                  return location.reload();
+                // inquiryData.add({
+                  //   index:index
+                // })
+              })
+            })
+            .catch(function(error) {
+              console.log("Error getting documents: ", error);
+              console.log(newestDisplayData)
+            });
+        } else if (this.sortValue === "oldest"){
+          const db = firebase.firestore();
+          const inquiryData = db.collection("inquiries")
+          const oldestDisplayData = inquiryData.orderBy('created').get()
+          .then(querySnapshot => {
+            querySnapshot.forEach((doc) => {
+                console.log(this.inquiryMultipleData.push(doc.data()));
+                localStorage.setItem("sortkey", this.sortValue)
+                return location.reload();
+              // inquiryData.add({
+              //   index:index
+              // })
+          })
+        })
+          .catch(function(error) {
+            console.log("Error getting documents: ", error);
+            console.log(oldestDisplayData)
+          });
+        } else {
+          console.log("sortError!")
+        }
+    },
+    deleteData: function(){
+      // alert('このお問い合わせを削除しますか？')
+      alert('テスト')
+      // location.href = "http://localhost:8080/inquirymail"
+    }
+  },
+  mounted: function(){
+    this.getData();
+  }
 }
 </script>
 
@@ -221,95 +295,16 @@ justify-content: space-around;
 
 .inquiry-delete{
   text-align:center;
-  border: 1px solid #797979;
-  border-radius: 10px;
   padding:5px 10px;
   margin: 0 10px;
 }
 
-.inquiry-delete:hover{
-  color: gray;
+.inquiry-delete button{
+   outline: none;
+}
+
+.inquiry-delete button:hover{
   cursor: pointer;
-}
-
-/* 再確認のホップアップ */
-.inquiry-popup{
-  font-size: 16px;
-}
-
-#inquiry_reconfirmation{
-  opacity: 1;
-  position: fixed;
-  background: #ffffff;
-  padding: 30px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-  top:50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  border-radius: 4px;
-  text-align: center;
-  transition: 0.4s;
-  z-index: 3;
-}
-
-#inquiry_reconfirmation.hidden {
-  display: none;
-}
-
-#inquiry_cancel{
-  width: 350px;
-  display: block;
-  text-decoration: none;
-  text-align: center;
-  padding:10px 0px;
-  margin: 28px auto 30px;
-  background-color:#fff;
-  color: #484b48;
-  border: 2px solid #484b48;
-  border-radius: 10px;
-  transition: background-color 0.2s linear;
-}
-
-#inquiry_cancel:hover{
-  cursor: pointer;
-  color: #fff;
-  background-color: #484b48;
-}
-
-#inquiry_btn{
-  width: 350px;
-  display: block;
-  text-decoration: none;
-  text-align: center;
-  padding:10px 0px;
-  margin: 28px auto 30px;
-  background-color:#fff;
-  color: #484b48;
-  border: 2px solid #484b48;
-  border-radius: 10px;
-  transition: background-color 0.2s linear;
-}
-
-#inquiry_btn:hover{
-  cursor: pointer;
-  color: #fff;
-  background-color: #484b48;
-}
-
-#reconfirmation_cover{
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: gainsboro;
-  z-index: 2;
-  opacity: 0.8;
-}
-
-#reconfirmation_cover.hidden{
-  transition: 0.4s;
-  display: none;
 }
 
 </style>
