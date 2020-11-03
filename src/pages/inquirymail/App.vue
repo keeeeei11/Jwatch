93e0fa6e2eeb3ff0dde9e264ad060483fb81903c
“kekekeigo1120” <t.keikei.0817@gmail.com>
2020-11-03 22:02:32 +0900

    ソート機能＆ページネーション機能実装

--- src/pages/inquirymail/App.vue
+++ src/pages/inquirymail/App.vue
@@ -15,48 +15,64 @@
   <main>
     <div class="inquiry-title">
       <h2>お問い合わせメール</h2>
+    <p>{{ inquiryMultipleData.length }} 件あります</p>
     </div>
-    <select name="" @change="sortData()" v-model="sortValue">
+    <div class="inquiry-sort">
+    <select @change="sortData()" v-model="sortValue">
       <option value="newest">新しい順</option>
       <option value="oldest">古い順</option>
     </select>
-    <div v-for="(inquirySingleData, index) in inquiryMultipleData" :key="inquirySingleData.index">
+    </div>
+    <!-- inquirySingleData = doc である。-->
+    <div v-for="(inquirySingleData) in getItems" :key="inquirySingleData.index">
       <div class="inquiry-contents">
       <div class="inquiry-example-contents">
         <div class="inquiry-basic-information">
           <div class="inquiry-basic-information-top">
               <div class="inquiry-name">
-                  <p>{{ inquirySingleData.name }}</p>
-                  <p>{{ index }}</p>
+                  <p>{{ inquirySingleData.data().name }}</p>
               </div>
               <div class="inquiry-email">
-                <p>{{ inquirySingleData.mailAddress }}</p>
+                <p>{{ inquirySingleData.data().mailAddress }}</p>
               </div>
           </div>
           <div class="inquiry-basic-information-bottom">
               <div class="inquiry-date">
-                  <p>{{ inquirySingleData.created }}</p>
+                  <p>{{ inquirySingleData.data().created }}</p>
               </div>
           </div>
         </div>
         <div class="inquiry-main-content">
             <div class="inquiry-title">
-                <p>{{ inquirySingleData.title }}</p>
+                <p>{{ inquirySingleData.data().title }}</p>
             </div>
             <div class="inquiry-text">
-                <p>{{ inquirySingleData.body }}</p>
+                <p>{{ inquirySingleData.data().body }}</p>
             </div>
         </div>
         <div class="inquiry-evaluation">
           <div class="inquiry-evaluation-contents">
               <div class="inquiry-delete">
-                <button @click="deleteData">このお問い合わせを削除する</button>
+                <button @click="deleteData(inquirySingleData.id)">このお問い合わせを削除する</button>
               </div>
           </div>
         </div>
       </div>
     </div>
     </div>
+    <Paginate :page-count="getPageCount"
+    :page-range="3"
+    :margin-pages="2"
+    :click-handler="clickCallback"
+    :prev-text="'<<'"
+    :next-text="'>>'"
+    :no-li-surround="true"
+    :container-class="'paginate'"
+    :prev-link-class="'prev-link'"
+    :page-link-class="'page-link'"
+    :next-link-class="'next-link'"
+    :active-class="'active-page-link'"
+    ></Paginate>
   <MoveTopBtn></MoveTopBtn>
   </main>
   </div>
@@ -72,36 +88,36 @@ import "firebase/firestore";
 import "firebase/storage";
 import myFirstMixin from '../../mixin/myFirstMixin';
 import MoveTopBtn from '../../components/MoveTopBtn.vue';
+import Paginate from 'vuejs-paginate'
 export default {
   props:{},
   data(){
     return{
       // お問い合わせのデータを格納
       inquiryMultipleData:[],
-      sortValue:localStorage.getItem("sortkey"),
+      sortValue:sessionStorage.getItem("sortkey"),
+      parPage: 5,
+      currentPage: 1
     }
   },
   mixins:[
     myFirstMixin
   ],
   components:{
-    MoveTopBtn
+    MoveTopBtn,
+    Paginate
   },
   methods:{
       // 初回訪問時&ページ更新時にデータを取得する
     getData: function(){
+      const db = firebase.firestore();
+      const inquiryData = db.collection("inquiries")
       if(this.sortValue === "newest"){
-        const db = firebase.firestore();
-        const inquiryData = db.collection("inquiries")
         const displayData = inquiryData.orderBy('created','desc').get()
         .then(querySnapshot => {
           // docにcloud firestoreからのデータが格納されている
             querySnapshot.forEach((doc) => {
-            console.log(this.inquiryMultipleData.push(doc.data()));
-              localStorage.setItem("sortkey", this.sortValue)
-            // inquiryData.add({
-            //   index:index
-            // })
+            console.log(this.inquiryMultipleData.push(doc))
           })
         })
         .catch(function(error) {
@@ -109,18 +125,11 @@ export default {
           console.log(displayData)
         });
       } else if (this.sortValue === "oldest"){
-          console.log(this.sortValue)
-          const db = firebase.firestore();
-          const inquiryData = db.collection("inquiries")
           const oldestDisplayData = inquiryData.orderBy('created').get()
           .then(querySnapshot => {
             querySnapshot.forEach((doc) => {
-                console.log(this.inquiryMultipleData.push(doc.data()));
-                localStorage.setItem("sortkey", this.sortValue)
-              // inquiryData.add({
-              //   index:index
-              // })
-      })
+                console.log(this.inquiryMultipleData.push(doc));
+          })
       })
       .catch(function(error) {
         console.log("Error getting documents: ", error);
@@ -130,20 +139,19 @@ export default {
         return
       }
     },
-    // selectタグの操作時に実行する。ソート後にページを更新してデータを表示させる。。
+    // selectタグの操作時に実行する。
+    // ソート後にページを更新(location.reload())してデータを表示させる部分だけ異なる。
     sortData: function(){
+      const db = firebase.firestore();
+        const inquiryData = db.collection("inquiries")
         if(this.sortValue === "newest"){
-            const db = firebase.firestore();
-            const inquiryData = db.collection("inquiries")
-            const newestDisplayData = inquiryData.orderBy('created','desc').get()
+          this.inquiryMultipleData = [];
+          const newestDisplayData = inquiryData.orderBy('created','desc').get()
             .then(querySnapshot => {
               querySnapshot.forEach((doc) => {
-                  console.log(this.inquiryMultipleData.push(doc.data()));
-                  localStorage.setItem("sortkey", this.sortValue)
+                console.log(this.inquiryMultipleData.push(doc));
+                  sessionStorage.setItem("sortkey", this.sortValue)
                   return location.reload();
-                // inquiryData.add({
-                  //   index:index
-                // })
               })
             })
             .catch(function(error) {
@@ -151,17 +159,13 @@ export default {
               console.log(newestDisplayData)
             });
         } else if (this.sortValue === "oldest"){
-          const db = firebase.firestore();
-          const inquiryData = db.collection("inquiries")
+          this.inquiryMultipleData = [];
           const oldestDisplayData = inquiryData.orderBy('created').get()
           .then(querySnapshot => {
-            querySnapshot.forEach((doc) => {
-                console.log(this.inquiryMultipleData.push(doc.data()));
-                localStorage.setItem("sortkey", this.sortValue)
+            querySnapshot.forEach(doc => {
+                console.log(this.inquiryMultipleData.push(doc));
+                sessionStorage.setItem("sortkey", this.sortValue)
                 return location.reload();
-              // inquiryData.add({
-              //   index:index
-              // })
           })
         })
           .catch(function(error) {
@@ -172,15 +176,43 @@ export default {
           console.log("sortError!")
         }
     },
-    deleteData: function(){
-      // alert('このお問い合わせを削除しますか？')
-      alert('テスト')
-      // location.href = "http://localhost:8080/inquirymail"
+    deleteData: function(id){
+      console.log(id);
+      if(confirm("このお問い合わせを削除しますか？一度削除すると2度と戻せません。")){
+        const db = firebase.firestore();
+        const inquiryData = db.collection("inquiries")
+          inquiryData.doc(id).delete()
+          .then(function(){
+            alert('削除できました。')
+            return location.reload();
+
+          })
+          .catch(function(error){
+            console.error("Error removing document: ", error);
+          })
+      }
+    },
+    clickCallback: function (pageNum) {
+      this.currentPage = Number(pageNum);
+      window.scrollTo({
+        top:0,
+        behavior:"instant",
+      })
+    },
+  },
+  computed: {
+    getItems: function() {
+      let current = this.currentPage * this.parPage;
+      let start = current - this.parPage;
+      return this.inquiryMultipleData.slice(start, current);
+    },
+    getPageCount: function() {
+      return Math.ceil(this.inquiryMultipleData.length / this.parPage);
     }
   },
   mounted: function(){
     this.getData();
-  }
+  },
 }
 </script>
 
@@ -226,7 +258,22 @@ main{
   text-align: center;
 }
 
-/*投稿例*/
+.inquiry-sort{
+  text-align: center;
+}
+
+.inquiry-sort select{
+  margin: 30px auto;
+  padding:10px 0;
+  width: 500px;
+  font-size: 18px;
+}
+
+.inquiry-sort select:hover{
+  cursor: pointer;
+}
+
+/*お問い合わせのフォーム*/
 .inquiry-example-contents{
   width: 70%;
   margin:30px auto;
@@ -305,6 +352,44 @@ justify-content: space-around;
 
 .inquiry-delete button:hover{
   cursor: pointer;
+}
+
+/* ページネーション */
+.paginate{
+  margin-top:20px;
+  text-align:center;
+}
+
+/* 前に戻るボタン */
+
+.prev-link{
+  font-size: 24px;
+  margin-right: 10px;
+  outline: none;
+  color:#484b48;
+}
+
+/* 数字のボタン */
+.page-link{
+  font-size: 24px;
+  font-weight: lighter;
+  padding:15px;
+  margin: 10px;
+  border:1px solid #484b48;
+  border-radius: 5px;
+  outline: none;
+}
+
+/* 次に進むボタン */
+.next-link{
+  font-size: 24px;
+  margin-left: 10px;
+  outline: none;
+}
+
+.active-page-link{
+  color: #ffffff;
+  background-color: gray;
 }
 
 </style>
