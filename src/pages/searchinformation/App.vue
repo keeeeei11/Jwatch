c3c2e529c5a58f7b1452729959b5573c971f9b67
“kekekeigo1120” <t.keikei.0817@gmail.com>
2020-11-04 22:01:15 +0900

    投稿の表示＆ページネーション機能の実装

--- src/pages/searchinformation/App.vue
+++ src/pages/searchinformation/App.vue
@@ -8,9 +8,8 @@
             <PageTitle title="Search informations" description="スタジアムとカテゴリーを選択すると情報が表示されます。"></PageTitle>
             <div class="select-stadium">
                 <form>
-                    <h2>スタジアムとカテゴリーを選択してください</h2><br>
-                    <!-- 送信ボタンを設置する -->
-                    <select v-model="stadium" class="stadium-list list" size="1"><br>
+                    <h2>スタジアムとカテゴリーを選択してボタンを押してください</h2><br>
+                    <select v-model="stadium" @change="noDataHide()" class="stadium-list list" size="1"><br>
                         <option value="" selected disabled>--スタジアム名を選択してください(必須)--</option>
                         <option value="" disabled>--北海道--</option>
                         <option value="[コンサドーレ札幌] 札幌厚別公園競技場">[コンサドーレ札幌] 札幌厚別公園競技場</option>
@@ -95,7 +94,7 @@
                     </select>
                 </form>
                 <form class="post-category">
-                    <select v-model="category" class="category-list list" size="1">
+                    <select v-model="category" @change="noDataHide()" class="category-list list" size="1">
                         <option value="" selected disabled>--カテゴリーを選択してください(必須)--</option>
                         <option value="スタジアムグルメ">スタジアムグルメ</option>
                         <option value="交通情報(駐車場や公共交通機関等)">交通情報(駐車場や公共交通機関等)</option>
@@ -105,28 +104,99 @@
                         <option value="その他">その他</option>
                     </select>
                 </form>
+                <button @click="sortData(stadium, category)">情報を見る！</button>
             </div>
-                <div class="stadium-basic-information">
-                    <div class="stadium-name">
-                        <h3> {{ stadium }} </h3>
-                    </div>
-                    <div class="category-name">
-                        <h3> {{ category }} </h3>
-                    </div>
+                <div class="post-sort">
+                    <select @change="sortData(stadium, category)" v-model="sortValue">
+                        <option value="newest" selected>日時が新しい順</option>
+                        <option value="oldest">日時が古い順</option>
+                        <option value="good">いいねが多い順</option>
+                    </select>
                 </div>
-                <div class="request-nav">
-                    <h3>求めている情報が見つからないときはこちら！</h3>
-                    <a href="https://jwatch-8411c.web.app/request/index.html">
-                        <div class="request-nav-contents">
-                            <div class="request-nav-ja">
-                                <p>リクエストする！</p>
+                <!-- 選択したスタジアムとカテゴリーで投稿がない時 -->
+                <div class="post-no-contents" v-if="noData">
+                    <p>{{ stadium }}の{{ category }}に関する投稿はまだありません…</p>
+                    <p>知っている情報があれば投稿して共有してみませんか？</p>
+                    <p>※GoogleまたはTwitterアカウントによるログイン,もしくは匿名ログインが必要です。</p>
+                    <a href="https://jwatch-8411c.web.app/posting/index.html">観戦情報を投稿する！</a>
+                </div>
+                <!-- 選択したスタジアムとカテゴリーで投稿がある時 -->
+                <div class="post-contents" v-else>
+                <div v-for="postSingleData in getItems" :key="postSingleData.index">
+                    <div class="post-example-contents">
+                        <div class="post-basic-information">
+                        <div class="post-basic-information-top">
+                            <div class="post-name">
+                                <p>{{ postSingleData.data().contributorName}}</p>
                             </div>
-                            <div class="request-nav-en">
-                                <p>Let’s request!</p>
+                            <div class="post-date">
+                                <p>{{ postSingleData.data().created }}</p>
                             </div>
                         </div>
-                    </a>
+                        <div class="post-basic-information-bottom">
+                            <div class="post-stadium">
+                                <p>{{ postSingleData.data().stadium }}</p>
+                            </div>
+                            <div class="post-category">
+                                <p>{{ postSingleData.data().category }}</p>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="post-main-content">
+                            <div class="post-title">
+                                <p>{{ postSingleData.data().title }}</p>
+                            </div>
+                            <div class="post-text">
+                                <p>{{ postSingleData.data().body }}</p>
+                            </div>
+                            <div class="post-img">
+                                <!-- <img src="../../assets/3602761_s.jpg" alt=""> -->
+                                <!-- <img src="../../assets/2396379_s.jpg" alt=""> -->
+                            </div>
+                        </div>
+                        <div class="post-evaluation">
+                            <div class="post-evaluation-contents">
+                                <div class="good-count evaluation-btn">
+                                    <button>いいね！ {{ postSingleData.data().likedCounter }}</button>
+                                </div>
+                                <!-- 条件分岐を実装する -->
+                                <div class="reporting evaluation-btn">
+                                    <button>通報する</button>
+                                </div>
+                                <div class="deleting evaluation-btn">
+                                    <button @click="deleteData(postSingleData.id)">削除する</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+            </div>
+            <Paginate :page-count="getPageCount"
+            :page-range="3"
+            :margin-pages="2"
+            :click-handler="clickCallback"
+            :prev-text="'<<'"
+            :next-text="'>>'"
+            :no-li-surround="true"
+            :container-class="'paginate'"
+            :prev-link-class="'prev-link'"
+            :page-link-class="'page-link'"
+            :next-link-class="'next-link'"
+            :active-class="'active-page-link'"
+            ></Paginate>
+            <div class="request-nav">
+                <h3>求めている情報が見つからないときはこちら！</h3>
+                <a href="https://jwatch-8411c.web.app/request/index.html">
+                    <div class="request-nav-contents">
+                        <div class="request-nav-ja">
+                            <p>リクエストする！</p>
+                        </div>
+                        <div class="request-nav-en">
+                            <p>Let’s request!</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
         </div>
     <MoveTopBtn></MoveTopBtn>
     </main>
@@ -138,7 +208,7 @@
 </template>
 
 <script>
-// import firebase from "firebase";
+import firebase from "firebase";
 import "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
@@ -148,23 +218,137 @@ import PageTitle from "../../components/PageTitle.vue"
 import MoveTopBtn from "../../components/MoveTopBtn.vue"
 import Jfooter from "../../components/Jfooter.vue"
 import myFirstMixin from "../../mixin/myFirstMixin";
+import Paginate from 'vuejs-paginate'
+
 
 export default {
+    prpos:{
+        },
     data(){
         return{
+            // ページ読み込み時はfalseに設定しておく
+            noData: false,
             stadium:"",
             category:"",
+            // 配列の取得
+            postMultipleData:[],
+            // ページネーション機能
+            sortValue:sessionStorage.getItem("sortkey"),
+            parPage: 10,
+            currentPage: 1,
         }
     },
-  components: {
-    Jheader,
-    PageTitle,
-    MoveTopBtn,
-    Jfooter,
-  },
-  mixins:[
-    myFirstMixin
-  ],
+    components: {
+        Jheader,
+        PageTitle,
+        MoveTopBtn,
+        Jfooter,
+        Paginate
+    },
+    mixins:[
+        myFirstMixin,
+    ],
+    methods:{
+        sortData: function(stadium, category){
+            // スタジアムとカテゴリーが入力されているかチェック
+        if(this.stadium.length > 0 && this.category.length > 0){
+            // 一度配列を空にしないと前の検索結果が残ったままになる。
+            this.postMultipleData = [];
+            // データの取得
+            const db = firebase.firestore();
+            const postData = db.collection("posts")
+            const displayData = postData.where("stadium", "==", stadium).where("category", "==", category)
+            // 0件の場合はcloud firestoreからデータを取得しないのでif文以下が実行されず、this.noData = trueで処理が終了する。
+            this.noData = true
+            // 新しい順が選択されている時
+            if(this.sortValue === "newest"){
+                const newestDisplayData = displayData.orderBy('created','desc').get()
+                .then(querySnapshot => {
+                    // console.log(this.querySnapshot.length)
+                    querySnapshot.forEach((doc) => {
+                        console.log(this.postMultipleData.push(doc));
+                        sessionStorage.setItem("sortkey", this.sortValue)
+                        // データが1件以上ある時はfalseにする
+                        this.noData = false
+                })
+                })
+                .catch(function(error) {
+                    console.log("Error getting documents: ", error);
+                    console.log(newestDisplayData)
+                });
+            // 古い順が選択されている時
+            } else if (this.sortValue === "oldest"){
+                const oldestDisplayData = displayData.orderBy('created').get()
+            .then(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                    console.log(this.postMultipleData.push(doc));
+                    sessionStorage.setItem("sortkey", this.sortValue)
+                        this.noData = false
+            })
+            })
+            .catch(function(error) {
+                console.log("Error getting documents: ", error);
+                console.log(oldestDisplayData)
+            });
+            // いいねが多い順が選択されている時
+            } else if (this.sortValue === "good"){
+                const goodDisplayData = displayData.orderBy('likedCounter', 'desc').get()
+            .then(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                    console.log(this.postMultipleData.push(doc));
+                    sessionStorage.setItem("sortkey", this.sortValue)
+                    this.noData = false
+            })
+            })
+            .catch(function(error) {
+                console.log("Error getting documents: ", error);
+                console.log(goodDisplayData)
+            });
+            } else {
+            console.log("sortError!")
+            }
+            // スタジアムとカテゴリーが選択されていない時に伝える
+        } else {
+            return alert("スタジアム名とカテゴリーを選択してください。")
+        }
+        },
+        deleteData: function(id){
+            console.log(id);
+            if(confirm("このお問い合わせを削除しますか？一度削除すると2度と戻せません。")){
+            const db = firebase.firestore();
+            const postData = db.collection("posts")
+            postData.doc(id).delete()
+            .then(function(){
+                alert('削除できました。')
+                return location.reload();
+            })
+            .catch(function(error){
+                console.error("エラーが発生しました。: ", error);
+            })
+        }
+        },
+        clickCallback: function (pageNum) {
+        this.currentPage = Number(pageNum);
+        window.scrollTo({
+            top:0,
+            behavior:"instant",
+        })
+        },
+        // スタジアム・カテゴリーを再選択した時に「情報を見るボタン」を押すまで、情報がないと誤表示するのを防ぐ役割
+        noDataHide:function(){
+            this.noData = false
+        }
+    },
+    computed: {
+      getItems: function() {
+        let current = this.currentPage * this.parPage;
+        let start = current - this.parPage;
+        return this.postMultipleData.slice(start, current);
+      },
+      getPageCount: function() {
+        return Math.ceil(this.postMultipleData.length / this.parPage);
+      },
+    },
 };
 </script>
 
@@ -188,10 +372,6 @@ main{
     display: block;
 }
 
-.select-stadium h2{
-    font-size: 21px;
-}
-
 .list{
     width: 70%;
     height: 40px;
@@ -202,13 +382,205 @@ main{
     background-color: #ffffff;
 }
 
+.select-stadium button{
+    /* width: 150px; */
+    font-size: 18px;
+    font-weight: 500;
+    color: #484b48;
+    text-decoration: none;
+    border:2px solid #484b48;
+    background-color: #fff;
+    margin:30px auto;
+    padding: 10px 20px;
+    border-radius: 10px;
+    transition: background-color 0.4s linear;
+}
+
+.select-stadium button:hover{
+    background-color: #484b48;
+    color: #fff;
+    transition: 0.4s;
+    cursor: pointer;
+}
+
+.select-stadium button:focus{
+    outline: none;
+}
+
 .stadium-basic-information{
     margin: 30px auto;
     text-align: center;
 }
 
-.stadium-basic-information h3{
-    font-size: 32px;
+.stadium-basic-information p{
+    font-size: 24px;
+}
+
+.post-sort{
+  text-align: center;
+}
+
+.post-sort select{
+  margin: 30px auto;
+  padding:10px 0;
+  width: 500px;
+  font-size: 18px;
+}
+
+.post-sort select:hover{
+  cursor: pointer;
+}
+
+.post-no-contents{
+    text-align: center;
+}
+
+.post-no-contents p{
+    font-size: 18px;
+}
+
+.post-no-contents a{
+    display: block;
+    width: 200px;
+    font-size: 18px;
+    font-weight: 500;
+    color: #484b48;
+    text-decoration: none;
+    border:2px solid #484b48;
+    background-color: #fff;
+    margin:30px auto 30px;
+    padding: 15px 20px;
+    border-radius: 10px;
+    transition: background-color 0.4s linear;
+}
+
+.post-no-contents a:hover{
+    background-color: #484b48;
+    color: #fff;
+    transition: 0.4s;
+    cursor: pointer;
+}
+
+/* 投稿を表示する部分 */
+.post-example-contents{
+  width: 70%;
+  margin:30px auto;
+  padding: 20px 40px 20px;
+  border: 2px solid #979797;
+  border-radius: 10px;
+}
+
+.post-basic-information{
+  display: block;
+}
+
+.post-basic-information-top{
+  display: flex;
+  height: 30px;
+  margin-bottom: 30px;
+  justify-content: space-around;
+}
+
+.post-basic-information-bottom{
+  display: flex;
+  justify-content: space-around;
+}
+
+.post-name{
+  font-size: 21px;
+}
+
+.post-stadium{
+  font-size: 18px;
+}
+
+.post-category{
+  font-size: 18px;
+}
+
+.post-date{
+  font-size: 18px;
+}
+
+.post-title p{
+  text-align: center;
+  font-size: 21px;
+}
+
+.post-text{
+  margin: 25px 0;
+  padding: 0 30px;
+  text-align: left;
+}
+
+.post-text p{
+  font-size: 18px;
+}
+
+.post-img{
+  display: flex;
+}
+
+.post-img img{
+  margin: 30px auto 20px;
+  width:30%;
+  height: auto;
+}
+
+.post-evaluation p{
+  margin-block-start: 0em;
+  margin-block-end: 0em;
+  font-size: 16px;
+}
+
+.post-evaluation-contents{
+  display: flex;
+  justify-content:center;
+}
+
+.evaluation-btn{
+  text-align:center;
+  border-radius: 10px;
+  padding:5px;
+  margin: 0 10px;
+}
+
+/* ページネーション */
+.paginate{
+  margin:100px;
+  text-align:center;
+}
+
+/* 前に戻るボタン */
+
+.prev-link{
+  font-size: 24px;
+  margin-right: 10px;
+  outline: none;
+  color:#484b48;
+}
+
+/* 数字のボタン */
+.page-link{
+  font-size: 24px;
+  font-weight: lighter;
+  padding:15px;
+  margin: 10px;
+  border:1px solid #484b48;
+  border-radius: 5px;
+  outline: none;
+}
+
+/* 次に進むボタン */
+.next-link{
+  font-size: 24px;
+  margin-left: 10px;
+  outline: none;
+}
+
+.active-page-link{
+  color: #ffffff;
+  background-color: gray;
 }
 
 /* リクエストするボタン */
@@ -242,6 +614,7 @@ main{
     font-size: 18px;
 }
 
+
 @media (max-width:959px ){
 /* メイン */
 /* スタジアム選択 */
@@ -249,13 +622,39 @@ main{
   margin-left: 0%;
 }
 
-.stadium-list{
+.list{
   width: 90%;
 }
 
 .stadium-basic-information h3{
   font-size: 28px;
   padding: 0px;
+}
+
+.post-sort select{
+    width: 60%;
+}
+
+/* 投稿内容 */
+.post-example-contents{
+  width: 80%;
+  margin: 10% auto;
+}
+
+.post-name{
+  font-size: 18px;
+}
+
+.post-stadium{
+  font-size: 16px;
+}
+
+.post-category{
+  font-size: 16px;
+}
+
+.post-date{
+  font-size: 16px;
 }
 
 /* カテゴリー選択 */
@@ -277,8 +676,52 @@ main{
   font-size: 18px;
 }
 
-.stadium-list{
+.list{
   font-size: 16px;
+}
+
+/* 投稿例 */
+.post-example-contents{
+  padding: 20px;
+}
+
+.post-title p{
+  text-align: center;
+  font-size: 18px;
+}
+.post-text{
+  margin:10px 0;
+  padding:0 10px;
+}
+
+.post-name{
+  font-size:18px;
+}
+
+.post-text p{
+  font-size: 16px;
+}
+
+.post-img{
+  /* display: block; */
+  flex-direction: column;
+}
+
+.post-img img{
+    margin: 20px auto;
+  width:60%;
+  height: auto;
+}
+
+.post-basic-information-bottom{
+  display: block;
+}
+.post-basic-information-bottom p{
+  text-align: center;
+}
+
+.post-evaluation p{
+  font-size: 14px;
 }
 
 /* カテゴリー選択 */
