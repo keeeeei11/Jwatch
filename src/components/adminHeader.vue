33ad0f538b4794b0da25c81bf5a301d9a1a9a1e4
“kekekeigo1120” <t.keikei.0817@gmail.com>
2020-11-08 17:40:51 +0900

    管理者ページのヘッダー実装

---
+++ src/components/adminHeader.vue
@@ -0,0 +1,44 @@
+<template>
+    <header>
+      <div class="header-contents">
+        <div class="header-title">
+          <a href="https://jwatch-8411c.web.app/management/index.html">管理者画面</a>
+        </div>
+        <div class="header-menu">
+          <a href="https://jwatch-8411c.web.app/reportlist/index.html">通報一覧</a>
+          <a href="https://jwatch-8411c.web.app/inquirymail/index.html">お問い合わせメール</a>
+        </div>
+      </div>
+    </header>
+</template>
+
+<style>
+header{
+  width: 100%;
+  position: fixed;
+  top:0;
+  background-color:white;
+  border-bottom: 1px solid #efefef;
+  z-index: 2;
+}
+.header-contents{
+  height: 50px;
+  display: flex;
+  margin: auto 50px;
+  justify-content: space-around;
+}
+
+.header-contents{
+  margin-top: 30px;
+}
+
+.header-contents a{
+  text-decoration: none;
+  color: #484b48;
+  margin: 0 30px;
+}
+
+.header-contents a:hover{
+  color: gray;
+}
+</style>
