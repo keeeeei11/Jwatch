7e79127e47fda827742b836fb6cad11aed763abb
“kekekeigo1120” <t.keikei.0817@gmail.com>
2020-11-20 16:17:53 +0900

    コードの整形

--- src/mixin/myFirstMixin.js
+++ src/mixin/myFirstMixin.js
@@ -6,48 +6,56 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
 export default {
-    // data内には最初に変数を定義する(初期値)
-    data(){
-        return{
-            visitorName:"",
-            email:"",
-            emailVerified:"",
-            photoURL:"",
-            isAnonymous:"",
-            visiorUid:"",
-            providerData:"",
-            isLogin:false,
+  // data内には最初に変数を定義する(初期値)
+  data() {
+    return {
+      visitorName: "",
+      email: "",
+      emailVerified: "",
+      photoURL: "",
+      isAnonymous: "",
+      visiorUid: "",
+      providerData: "",
+      isLogin: false,
+    };
+  },
+  mounted: function() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyAb7dyC4OAKSZaaWHLIS2SlskncyESE6Lg",
+      authDomain: "jwatch-8411c.firebaseapp.com",
+      databaseURL: "https://jwatch-8411c.firebaseio.com",
+      projectId: "jwatch-8411c",
+      storageBucket: "jwatch-8411c.appspot.com",
+      messagingSenderId: "868452645100",
+      appId: "1:868452645100:web:b0ac64ce8f42b75a7dcdc9",
+      measurementId: "G-RK4728SNM4",
+    };
+    // 稀にfirebaseの初期化が多重に行われてエラーが発生するのでそれを防ぐ
+    if (firebase.apps.length === 0) {
+      firebase.initializeApp(firebaseConfig);
+      firebase.analytics();
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          // ログイン時
+          this.visitorName = user.displayName;
+          this.email = user.email;
+          this.emailVerified = user.emailVerified;
+          this.photoURL = user.photoURL;
+          this.isAnonymous = user.isAnonymous;
+          this.visitorUid = user.uid;
+          this.providerData = user.providerData;
+          this.isLogin = true;
+          console.log(
+            this.visitorName,
+            this.visitorUid,
+            this.email,
+            this.emailVerified,
+            this.photoURL,
+            this.isAnonymous,
+            this.providerData
+          );
         }
-    },
-    mounted:function(){
-        const firebaseConfig = {
-        apiKey: "AIzaSyAb7dyC4OAKSZaaWHLIS2SlskncyESE6Lg",
-        authDomain: "jwatch-8411c.firebaseapp.com",
-        databaseURL: "https://jwatch-8411c.firebaseio.com",
-        projectId: "jwatch-8411c",
-        storageBucket: "jwatch-8411c.appspot.com",
-        messagingSenderId: "868452645100",
-        appId: "1:868452645100:web:b0ac64ce8f42b75a7dcdc9",
-        measurementId: "G-RK4728SNM4"
-      };
-      // 稀にfirebaseの初期化が多重に行われてエラーが発生するのでそれを防ぐ
-      if (firebase.apps.length === 0) {
-        firebase.initializeApp(firebaseConfig);
-        firebase.analytics();
-        firebase.auth().onAuthStateChanged(user => {
-          if (user) {
-            // ログイン時
-            this.visitorName = user.displayName;
-            this.email = user.email;
-            this.emailVerified = user.emailVerified;
-            this.photoURL = user.photoURL;
-            this.isAnonymous = user.isAnonymous;
-            this.visitorUid = user.uid;
-            this.providerData = user.providerData;
-            this.isLogin = true
-            console.log(this.visitorName, this.visitorUid ,this.email ,this.emailVerified ,this.photoURL ,this.isAnonymous  ,this.providerData)
-          }
-        })
-      }
+      });
     }
-}
+  },
+};
