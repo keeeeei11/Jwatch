43f5e6435bb8450a2c64d09d264342d574543197
“kekekeigo1120” <t.keikei.0817@gmail.com>
2020-11-26 15:08:01 +0900

    カテゴリー選択バーのコンポーネント化

---
+++ src/components/InputCategory.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="input-category">
+    <h3>カテゴリー</h3>
+    <select @change="inputCategory" size="1">
+      <option value="" selected disabled
+        >--カテゴリーを選択してください(必須)--</option
+      >
+      <option value="スタジアムグルメ">スタジアムグルメ</option>
+      <option value="交通情報(駐車場や公共交通機関等)"
+        >交通情報(駐車場や公共交通機関等)</option
+      >
+      <option value="座席(座席確保・座席の特徴・見やすさ)"
+        >座席(座席確保・座席の特徴・見やすさ)</option
+      >
+      <option value="周辺情報(飲食店・ホテル・観光地等)"
+        >周辺情報(飲食店・ホテル・観光地等)</option
+      >
+      <option value="チーム特有の情報(ダービーなど)"
+        >チーム特有の情報(ダービーなど)</option
+      >
+      <option value="その他">その他</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    value: { type: String, required: true },
+  },
+  methods: {
+    inputCategory: function(e) {
+      this.$emit("input", e.target.value);
+    }
+  },
+}
+</script>
+
+<style>
+h3 {
+  font-size: 21px;
+  font-weight: normal;
+}
+
+select {
+  width: 60%;
+  height: 40px;
+  font-size: 18px;
+  margin-bottom: 40px;
+}
+@media (max-width: 959px) {
+  select {
+    width: 90%;
+  }
+}
+
+@media (max-width: 559px) {
+  h3 {
+    font-size: 18px;
+  }
+
+  select {
+    font-size: 16px;
+  }
+}
+</style>
