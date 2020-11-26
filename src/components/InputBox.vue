3d3abe10c65c20639e69150ffaee7782bab34029
“kekekeigo1120” <t.keikei.0817@gmail.com>
2020-11-26 15:40:36 +0900

    ファイル名変更&タイトルをプロパティに変更

---
+++ src/components/InputBox.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h3> {{ subject }}</h3>
+   <input
+   :type= "type"
+   :name= "name"
+   :placeholder= "placeholder"
+   :maxlength= "maxlength"
+   @input="inputValue"
+   >
+   </div>
+</template>
+
+<script>
+export default {
+  props:{
+    subject:{type:String, required:false},
+    type:{type:String, required:false},
+    name:{type:String, required:false},
+    placeholder:{type:String, default:"20字以内で入力してください"},
+    // 字数調整はdefault内で行う
+    maxlength:{type:String, default:"20"}
+  },
+  methods:{
+    inputValue: function(e){
+      this.$emit("input", e.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  /* 題名 */
+  h3 {
+    font-size: 21px;
+    font-weight: normal;
+  }
+
+  input{
+    width: 60%;
+    height: 40px;
+    font-size: 18px;
+  }
+
+@media (max-width: 959px) {
+   /* 題名 */
+  input{
+    width: 90%;
+  }
+}
+
+@media (max-width: 559px) {
+  h3 {
+    font-size: 18px;
+  }
+}
+</style>
---
+++ src/components/TextareaBox.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="input-body">
+    <h3>{{ subject }}</h3>
+    <textarea
+    v-model="count"
+    :placeholder= "placeholder"
+    :maxlength= "maxlength"
+    @input="inputValue"
+    ></textarea>
+    <div class="count-character">
+      <p>残り{{ 400 - count.length }}字です</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      count:"",
+    }
+  },
+  props:{
+    subject:{type:String, required:false},
+    placeholder:{type:String, default:"400字以内で入力してください"},
+    // 字数調整はdefault内で行う
+    maxlength:{type:String, default:"400"}
+  },
+  methods:{
+    inputValue: function(e){
+      this.$emit("input", e.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  /* 題名 */
+h3 {
+  font-size: 21px;
+  font-weight: normal;
+}
+
+textarea {
+  width: 60%;
+  height: 400px;
+}
+
+.count-character p{
+  font-size: 18px;
+}
+
+@media (max-width: 959px) {
+  textarea {
+    width: 90%;
+  }
+}
+
+@media (max-width: 559px) {
+  h3 {
+    font-size: 18px;
+  }
+  .count-character p{
+    font-size: 16px;
+  }
+}
+</style>
