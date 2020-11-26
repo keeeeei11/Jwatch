53f93014ccb96957f54cc7345b97e7b2f0a96082
“kekekeigo1120” <t.keikei.0817@gmail.com>
2020-11-26 12:11:32 +0900

    inputタグのコンポーネント化

---
+++ src/components/InputTitle.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h3>タイトル</h3>
+   <input
+   :type= "type"
+   :name= "name"
+   :placeholder= "placeholder"
+   :maxlength= "maxlength"
+   @input="inputValue"
+   >
+   </div>
+</template>
+
+<script>
+export default {
+  props:{
+    type:{type:String, required:false},
+    name:{type:String, required:false},
+    placeholder:{type:String, default:"20字以内で入力してください"},
+    maxlength:{type:String, default:"20"}
+  },
+  methods:{
+    inputValue: function(e){
+      this.$emit("input", e.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  /* 題名 */
+  h3 {
+    font-size: 21px;
+    font-weight: normal;
+  }
+
+  input{
+    width: 60%;
+    height: 40px;
+    font-size: 18px;
+  }
+
+@media (max-width: 959px) {
+   /* 題名 */
+  input{
+    width: 90%;
+  }
+}
+
+@media (max-width: 559px) {
+  h3 {
+    font-size: 18px;
+  }
+}
+</style>
