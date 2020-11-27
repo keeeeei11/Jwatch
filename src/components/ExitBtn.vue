205776991ae82c932fe818b5054bdeeab309abf5
“kekekeigo1120” <t.keikei.0817@gmail.com>
2020-11-27 09:10:02 +0900

    マイページのログアウト＆アカウント削除ボタンのコンポーネント化

---
+++ src/components/ExitBtn.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <p>
+      {{ execution }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    execution:String
+  }
+
+}
+</script>
+
+<style scoped>
+p{
+  font-size: 16px;
+  width: 250px;
+  text-align: center;
+  margin: 70px auto;
+  padding: 20px 45px;
+  color: #484b48;
+  text-decoration: none;
+  border: 2px solid #484b48;
+  background-color: #fff;
+  border-radius: 10px;
+  transition: background-color 0.3s linear;
+}
+
+p:hover {
+  background-color: #484b48;
+  color: #fff;
+  transition: 0.3s;
+  cursor: pointer;
+}
+
+@media (max-width: 559px) {
+  p{
+    font-size: 16px;
+    width: 200px;
+    margin:30px auto;
+  }
+}
+</style>
