33604540071783339606ca7fa442d822da71a1d9
“kekekeigo1120” <t.keikei.0817@gmail.com>
2020-12-01 16:50:54 +0900

    投稿データの表示を表す部分のコンポーネント化

---
+++ src/components/DisplayNoData.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="post-no-contents">
+    <p>{{ stadium }}の{{ category }}に関する投稿はまだありません…</p>
+    <p>知っている情報があれば投稿して共有してみませんか？</p>
+    <p>
+      ※GoogleまたはTwitterアカウントによるログイン,もしくは匿名ログインが必要です。
+    </p>
+    <a href="https://jwatch-8411c.web.app/posting/index.html"
+      >観戦情報を投稿する！</a
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    stadium:String,
+    category:String
+  }
+}
+</script>
+
+<style scoped>
+/* 投稿が0件の時に表示する */
+.post-no-contents {
+  text-align: center;
+  padding: 30px;
+}
+
+.post-no-contents p {
+  font-size: 18px;
+}
+
+.post-no-contents a {
+  display: block;
+  width: 200px;
+  font-size: 18px;
+  font-weight: 500;
+  color: #484b48;
+  text-decoration: none;
+  border: 2px solid #484b48;
+  background-color: #fff;
+  margin: 30px auto 30px;
+  padding: 15px 20px;
+  border-radius: 10px;
+  transition: background-color 0.4s linear;
+}
+
+.post-no-contents a:hover {
+  background-color: #484b48;
+  color: #fff;
+  transition: 0.4s;
+  cursor: pointer;
+}
+
+@media (max-width: 559px) {
+  .post-no-contents p {
+    font-size: 16px;
+  }
+  .post-no-contents a {
+      font-size: 16px;
+  }
+}
+</style>
