9aeef74cba71f7fb9bd0b418f38f63a3a8f8aa38
“kekekeigo1120” <t.keikei.0817@gmail.com>
2020-12-08 20:36:19 +0900

    コードの整形

--- src/mixin/myFirstMixin.js
+++ src/mixin/myFirstMixin.js
@@ -1,30 +1,30 @@
-import firebase from "firebase";
-import "firebase/auth";
-import "firebase/firestore";
-import "firebase/storage";
+import firebase from "firebase/app";
+import               "firebase/auth";
+import               "firebase/firestore";
+import               "firebase/storage";
 export default {
   data() {
     return {
-      visitorName: "",
-      visiorUid: "",
-      email: "",
+      visitorName:   "",
+      visiorUid:     "",
+      email:         "",
       emailVerified: "",
-      photoURL: "",
-      isAnonymous: "",
-      providerData: "",
-      isLogin: false,
+      photoURL:      "",
+      isAnonymous:   "",
+      providerData:  "",
+      isLogin:       false,
     };
   },
   mounted: function() {
     const firebaseConfig = {
-      apiKey: "AIzaSyAb7dyC4OAKSZaaWHLIS2SlskncyESE6Lg",
-      authDomain: "jwatch-8411c.firebaseapp.com",
-      databaseURL: "https://jwatch-8411c.firebaseio.com",
-      projectId: "jwatch-8411c",
-      storageBucket: "jwatch-8411c.appspot.com",
+      apiKey:            "AIzaSyAb7dyC4OAKSZaaWHLIS2SlskncyESE6Lg",
+      authDomain:        "jwatch-8411c.firebaseapp.com",
+      databaseURL:       "https://jwatch-8411c.firebaseio.com",
+      projectId:         "jwatch-8411c",
+      storageBucket:     "jwatch-8411c.appspot.com",
       messagingSenderId: "868452645100",
-      appId: "1:868452645100:web:b0ac64ce8f42b75a7dcdc9",
-      measurementId: "G-RK4728SNM4",
+      appId:             "1:868452645100:web:b0ac64ce8f42b75a7dcdc9",
+      measurementId:     "G-RK4728SNM4"
     };
     // 稀にfirebaseの初期化が多重に行われてエラーが発生するのでそれを防ぐ
     if (firebase.apps.length === 0) {
@@ -33,14 +33,14 @@ export default {
       firebase.auth().onAuthStateChanged((user) => {
         if (user) {
           // ログイン時
-          this.visitorName = user.displayName;
-          this.email = user.email;
+          this.email         = user.email;
           this.emailVerified = user.emailVerified;
-          this.photoURL = user.photoURL;
-          this.isAnonymous = user.isAnonymous;
-          this.visitorUid = user.uid;
-          this.providerData = user.providerData;
-          this.isLogin = true;
+          this.isAnonymous   = user.isAnonymous;
+          this.isLogin       = true;
+          this.photoURL      = user.photoURL;
+          this.providerData  = user.providerData;
+          this.visitorName   = user.displayName;
+          this.visitorUid    = user.uid;
           console.log(
             this.visitorName,
             this.visitorUid,
@@ -52,7 +52,7 @@ export default {
           );
         } else {
           this.visitorName = null;
-          this.visitorUid = null
+          this.visitorUid  = null
         }
         // 匿名ユーザーが投稿した時にユーザー名を「匿名」と表示する
         if(this.isAnonymous == true){
